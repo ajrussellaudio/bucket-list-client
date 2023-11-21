1e13e1139214d8a56513d297699a298f195d4bd0
Alan Russell <ajrussellaudio@gmail.com>
2023-11-21 16:01:28 +0000

    Query Countries from customhook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useCountries } from "./hooks/useCountries";
+
 function App() {
-  return <></>;
+  const { isLoading, countries } = useCountries();
+
+  return isLoading ? (
+    <>
+      <h1>Loading...</h1>
+    </>
+  ) : (
+    <>
+      <pre>{JSON.stringify(countries, null, 2)}</pre>
+    </>
+  );
 }
 
 export default App;
